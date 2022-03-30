456ef8f0a3924412d144259ec8aac797a5c1ba79
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2022-03-30 22:40:24 +0200

    sidebar

--- app/views/inc/sidebar.php
+++ app/views/inc/sidebar.php
@@ -1,17 +1,14 @@
-<?php
-class Contact extends View {
-	public function output(){
-		$title= $this->model->title;
-		// require APPROOT . '/views/inc/header.php';
-		
-		echo "
-		<meta charset='UTF-8'>
-  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-  <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-  <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x' crossorigin='anonymous'>
-  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css' integrity='undefined' crossorigin='anonymous'>
-  <link rel='stylesheet' href='<?php echo URLROOT; ?>css/style.css'>
- <svg xmlns='http://www.w3.org/2000/svg' style='display: none;'>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="undefined" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>css/style.css">
+  <title><?php echo SITENAME; ?></title>
+</head>
+<svg xmlns='http://www.w3.org/2000/svg' style='display: none;'>
 		<symbol id='bootstrap' viewBox='0 0 118 94'>
 		  <title>Bootstrap</title>
 		  <path fill-rule='evenodd' clip-rule='evenodd' d='M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z'></path>
@@ -69,7 +66,7 @@ class Contact extends View {
 		  <path fill-rule='evenodd' d='M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z'></path>
 		</symbol>
 	  </svg>
-	  <div class='d-flex flex-column flex-shrink-0 p-3 text-white bg-dark' style='width: 280px; height: 100%; margin-right: 800 float'>
+	<div class='d-flex flex-column flex-shrink-0 p-3 text-white bg-dark' style='width: 280px; height: 100%;'>
 		<a href='/' class='d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none'>
 		  <svg class='bi me-2' width='40' height='32'><use xlink:href='#bootstrap'></use></svg>
 		  <span class='fs-4'>Sidebar</span>
@@ -83,7 +80,7 @@ class Contact extends View {
 			</a>
 		  </li>
 		  <li>
-			<a href='#' class='nav-link text-white'>
+			<a href="<?php echo URLROOT . 'pages/contact'; ?>" class='nav-link text-white'>
 			  <svg class='bi me-2' width='16' height='16'><use xlink:href='#speedometer2'></use></svg>
 			  Dashboard
 			</a>
@@ -121,10 +118,5 @@ class Contact extends View {
 			<li><a class='dropdown-item' href='#'>Sign out</a></li>
 		  </ul>
 		</div>
-	  </div>";
-		require APPROOT . '/views/inc/footer.php';
-	}
-	
-}
-
-?>
+	  </div>
+      <div class="container">

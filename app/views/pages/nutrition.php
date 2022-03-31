57556628be5916be03ef80d039ac273b27591211
Abdelrahman Elewa <95569208+AbdelrahmanElewaa@users.noreply.github.com>
2022-04-01 01:33:14 +0200

    Add files via upload

--- app/views/pages/nutrition.php
+++ app/views/pages/nutrition.php
@@ -4,8 +4,22 @@ class nutrition extends view {
 	 require APPROOT.'/views/inc/header.php';
 	
 	 ?>
+<form action="/action_page.php">
 
-
+	<br><h2>Nutrition Plan</h2>
+	<hr>
+  <label for="Date">Day Date:</label>
+  <input  class="form-control" type="date" id="birthday" name="birthday"><br>
+  <input type="text"  class="form-control" placeholder="Meal Name">
+  <br>
+  <input type="text"  class="form-control"placeholder="quantity"><br>
+  <input type="text"  class="form-control"placeholder="Calories"><br>
+  <input type="text" class="form-control" placeholder="Carbs"><br>
+  <input type="text"  class="form-control"placeholder="Fats"><br>
+  <input type="text" class="form-control" placeholder="protein"><br>
+  <input type="text" class="form-control" placeholder="Time"><br>
+  <input type="submit" value="Add" class="btn btn-primary">
+</form>
 
 <?php
 	require APPROOT.'/views/inc/footer.php';

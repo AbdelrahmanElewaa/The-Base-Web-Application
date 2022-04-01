b2750b9b80873dc2772ad7a0caa48ccf55eae40e
Abdelrahman Elewa <abdelrahmanelewa6@gmail.com>
2022-04-01 02:09:39 +0200

    Update nutrition.php

--- app/views/pages/nutrition.php
+++ app/views/pages/nutrition.php
@@ -39,35 +39,4 @@ class nutrition extends view {
 	
 }
 
-=======
-<?php
-class nutrition extends view {
-	public function output(){
-	 require APPROOT.'/views/inc/header.php';
-	
-	 ?>
-<form action="/action_page.php">
-
-	<br><h2>Nutrition Plan</h2>
-	<hr>
-  <label for="Date">Day Date:</label>
-  <input  class="form-control" type="date" id="birthday" name="birthday"><br>
-  <input type="text"  class="form-control" placeholder="Meal Name">
-  <br>
-  <input type="text"  class="form-control"placeholder="quantity"><br>
-  <input type="text"  class="form-control"placeholder="Calories"><br>
-  <input type="text" class="form-control" placeholder="Carbs"><br>
-  <input type="text"  class="form-control"placeholder="Fats"><br>
-  <input type="text" class="form-control" placeholder="protein"><br>
-  <input type="text" class="form-control" placeholder="Time"><br>
-  <input type="submit" value="Add" class="btn btn-primary">
-</form>
-
-<?php
-	require APPROOT.'/views/inc/footer.php';
-	}
-	
-}
-
->>>>>>> 57556628be5916be03ef80d039ac273b27591211
-?>
+ 

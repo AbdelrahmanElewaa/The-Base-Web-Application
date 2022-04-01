b8601f7470045120c4c43f6836049cdddbe2d863
Abdelrahman Elewa <95569208+AbdelrahmanElewaa@users.noreply.github.com>
2022-04-01 02:01:57 +0200

    Add files via upload

--- app/views/pages/addnutrition.php
+++ app/views/pages/addnutrition.php
@@ -1,47 +1,5 @@
-<<<<<<< HEAD
 <?php
-class nutrition extends view {
-	public function output(){
-	 require APPROOT.'/views/inc/header.php';
-	
-	 ?>
-	 <br><h2>Nutrition Plan</h2>
-	<hr>
-	 <br>
- <a href="<?php echo URLROOT.'pages/addnutrition'; ?>" class="btn btn-success">Add Nutrition Plan</a>
-<br>
-<div>
- <table class="table table-success table-striped">
-  <thead>
-  <th>Date</th>
-	  <th>Meal Name</th>
-	  <th>Quantity</th>
-	  <th>Calories</th>
-	  <th>Carbs</th>
-	  <th>Fats</th>
-	  <th>Protein</th>
-	  <th>Time</th>
-	</thead>
-	<td>30/1/2021</td>
-	<td>Salad</td>
-	<td>400 grams</td>
-	<td>300 cal</td>
-	<td>100</td>
-	<td>50</td>
-	<td>30</td>
-	<td>BreakFast</td>
-	 
-</table>
-	</div>
-<?php
-	require APPROOT.'/views/inc/footer.php';
-	}
-	
-}
-
-=======
-<?php
-class nutrition extends view {
+class addnutrition extends view {
 	public function output(){
 	 require APPROOT.'/views/inc/header.php';
 	
@@ -69,5 +27,4 @@ class nutrition extends view {
 	
 }
 
->>>>>>> 57556628be5916be03ef80d039ac273b27591211
 ?>

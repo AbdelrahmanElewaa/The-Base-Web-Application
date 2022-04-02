7710b6d42452e0434a0a2877a89d8ad3b7b744c9
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2022-04-02 16:31:39 +0200

    Revert "Add files via upload"
    This reverts commit fc42c0470736efb12cf279f6c80676bc17990c31.

--- app/views/pages/Index.php
+++ app/views/pages/Index.php
@@ -5,188 +5,20 @@ class Index extends View
   {
     $title = $this->model->title;
     $subtitle = $this->model->subtitle;
-    // $user_id = $_SESSION['user_id'];
-    // $user_name = $_SESSION['user_name'];
+    $user_id = $_SESSION['user_id'];
+    $user_name = $_SESSION['user_name'];
 
     require APPROOT . '/views/inc/header.php';
     $text = <<<EOT
-    <!DOCTYPE html>
-    <html style="font-size: 16px;">
-      <head>
-      <style>
-      background-image: url("MVC/public/images/pexels-victor-freitas-2261477.jpg"); 
-
-      </style>
-
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta charset="utf-8">
-        <meta name="keywords" content="Sport Club, Weight Loss &amp;amp; Diet Plan, Pilates &amp;amp; yoga classes, How We Help, 01, 02, About Club, Run smart, hurt less, Lear more about our
-    sport club, INTUITIVE">
-        <meta name="description" content="">
-       
-        <section class="u-clearfix u-image u-valign-middle u-section-17" id="carousel_8b4c" data-image-width="1600" data-image-height="1067">
-          <div class="u-align-left u-container-style u-expanded-width-xs u-group u-opacity u-opacity-55 u-shape-rectangle u-white u-group-1">
-            <div class="u-container-layout u-valign-middle u-container-layout-1">
-              <h1 class="u-text u-title u-text-11">Fitness Program</h1>
-              <p class="u-large-text u-text u-text-variant u-text-22">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
-              <p class="u-text u-text-default u-text-33">Image from <a href="https://www.freepik.com/free-photos/woman" class="u-border-2 u-border-white u-btn u-button-link u-button-style u-none u-text-body-alt-color u-btn-1">Freepik</a>
-              </p>
-              <a href="https://nicepage.com/k/auto-mechanic-html-templates" class="u-active-white u-border-2 u-border-active-white u-border-hover-white u-border-white u-btn u-button-style u-custom-color-1 u-hover-white u-text-active-palette-1-dark-2 u-text-body-alt-color u-text-hover-palette-1-dark-1 u-btn-2">About us</a>
-            </div>
-          </div>
-        </section>
-        <section class="u-align-center u-clearfix u-palette-5-light-3 u-section-2222" id="carousel_ef93">
-          <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
-            <div class="u-clearfix u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-gutter-0 u-layout-wrap u-layout-wrap-1">
-              <div class="u-layout">
-                <div class="u-layout-row">
-                  <div class="u-size-15 u-size-60-md">
-                    <div class="u-layout-col">
-                      <div class="u-container-style u-layout-cell u-left-cell u-size-60 u-layout-cell-1">
-                        <div class="u-container-layout u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-container-layout-1"><span class="u-icon u-icon-circle u-opacity u-opacity-40 u-text-palette-1-dark-1 u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 95.333 95.332" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-1d50"></use></svg><svg class="u-svg-content" viewBox="0 0 95.333 95.332" x="0px" y="0px" id="svg-1d50" style="enable-background:new 0 0 95.333 95.332;"><g><g><path d="M30.512,43.939c-2.348-0.676-4.696-1.019-6.98-1.019c-3.527,0-6.47,0.806-8.752,1.793    c2.2-8.054,7.485-21.951,18.013-23.516c0.975-0.145,1.774-0.85,2.04-1.799l2.301-8.23c0.194-0.696,0.079-1.441-0.318-2.045    s-1.035-1.007-1.75-1.105c-0.777-0.106-1.569-0.16-2.354-0.16c-12.637,0-25.152,13.19-30.433,32.076    c-3.1,11.08-4.009,27.738,3.627,38.223c4.273,5.867,10.507,9,18.529,9.313c0.033,0.001,0.065,0.002,0.098,0.002    c9.898,0,18.675-6.666,21.345-16.209c1.595-5.705,0.874-11.688-2.032-16.851C40.971,49.307,36.236,45.586,30.512,43.939z"></path><path d="M92.471,54.413c-2.875-5.106-7.61-8.827-13.334-10.474c-2.348-0.676-4.696-1.019-6.979-1.019    c-3.527,0-6.471,0.806-8.753,1.793c2.2-8.054,7.485-21.951,18.014-23.516c0.975-0.145,1.773-0.85,2.04-1.799l2.301-8.23    c0.194-0.696,0.079-1.441-0.318-2.045c-0.396-0.604-1.034-1.007-1.75-1.105c-0.776-0.106-1.568-0.16-2.354-0.16    c-12.637,0-25.152,13.19-30.434,32.076c-3.099,11.08-4.008,27.738,3.629,38.225c4.272,5.866,10.507,9,18.528,9.312    c0.033,0.001,0.065,0.002,0.099,0.002c9.897,0,18.675-6.666,21.345-16.209C96.098,65.559,95.376,59.575,92.471,54.413z"></path>
-    </g>
-    </g></svg></span>
-                          <p class="u-text u-text-11">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="u-size-24 u-size-40-md">
-                    <div class="u-layout-col">
-                      <div class="u-container-style u-layout-cell u-size-60 u-layout-cell-2">
-                        <div class="u-container-layout u-valign-bottom-lg u-valign-bottom-sm u-container-layout-2">
-                          <img src="/MVC/public/images/pexels-daria-shevtsova-949069.jpg" alt="" class="u-expanded-width u-image u-image-default u-image-1" data-image-width="1200" data-image-height="1600">
-                          <img alt="" class="u-image u-image-default u-image-2" data-image-width="1067" data-image-height="1600" src="/MVC/public/images/pexels-polina-tankilevitch-3872373.jpg">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="u-size-20-md u-size-21">
-                    <div class="u-layout-col">
-                      <div class="u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-3">
-                        <div class="u-container-layout u-container-layout-3">
-                          <h1 class="u-custom-font u-text u-text-black u-text-33"> We​ight<br>&nbsp;Loss&nbsp; &amp;
-                          </h1>
-                          <h1 class="u-custom-font u-text u-text-black u-text-33">Nutrition&nbsp; plan</h1>
-                        </div>
-                      </div>
-                      <div class="u-container-style u-layout-cell u-right-cell u-shape-rectangle u-size-30 u-layout-cell-4">
-                        <div class="u-container-layout u-container-layout-4">
-                          <p class="u-text u-text-default u-text-55">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
-                          <a href="https://nicepage.app" class="u-active-none u-border-2 u-border-active-palette-1-dark-1 u-border-hover-palette-1-dark-1 u-border-palette-1-dark-1 u-btn u-button-style u-hover-none u-none u-text-active-black u-text-custom-color-1 u-text-hover-palette-1-dark-1 u-btn-1">learn more</a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section class="u-clearfix u-palette-5-light-3 u-section-3333" id="carousel_2b2f">
-          <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
-            <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
-              <div class="u-layout">
-                <div class="u-layout-row">
-                  <div class="u-container-style u-layout-cell u-left-cell u-size-20 u-layout-cell-1">
-                    <div class="u-container-layout u-valign-top u-container-layout-1">
-                      <h1 class="u-text u-text-custom-color-1 u-text-11">Trai<span style="font-weight: 700;"></span>nging&nbsp;<br>Programs
-                      </h1>
-                    </div>
-                  </div>
-                  <div class="u-container-style u-layout-cell u-right-cell u-size-40 u-layout-cell-2">
-                    <div class="u-container-layout u-valign-top u-container-layout-2">
-                      <p class="u-text u-text-22">Quis hendrerit dolor magna eget est lorem. Convallis posuere morbi leo urna molestie at elementum. Risus in hendrerit gravida rutrum quisque non tellus orci. Neque volutpat ac tincidunt vitae semper quis. Vitae semper quis lectus nulla at volutpat diam. Suscipit tellus mauris a diam. </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="u-expanded-width u-gallery u-layout-grid u-lightbox u-show-text-on-hover u-gallery-11">
-              <div class="u-gallery-inner u-gallery-inner-ONE">
-                <div class="u-effect-fade u-gallery-item u-gallery-item-ONE">
-                  <div class="u-back-slide" data-image-width="1600" data-image-height="1600">
-                    <img class="u-back-image u-expanded" src="/MVC/public/images/IMG_1140.jpg">
-                  </div>
-                  <div class="u-over-slide u-shading u-over-slide-1">
-                    <h3 class="u-gallery-heading"></h3>
-                    <p class="u-gallery-text"></p>
-                  </div>
-                </div>
-                <div class="u-effect-fade u-gallery-item u-gallery-item-TWO">
-                  <div class="u-back-slide" data-image-width="1600" data-image-height="1600">
-                    <img class="u-back-image u-expanded" src="/MVC/public/images/IMG_1075.jpg">
-                  </div>
-                  <div class="u-over-slide u-shading u-over-slide-2">
-                    <h3 class="u-gallery-heading"></h3>
-                    <p class="u-gallery-text"></p>
-                  </div>
-                </div>
-                <div class="u-effect-fade u-gallery-item u-gallery-item-THREE">
-                  <div class="u-back-slide" data-image-width="1600" data-image-height="1600">
-                    <img class="u-back-image u-expanded" src="/MVC/public/images/IMG_08231.jpg">
-                  </div>
-                  <div class="u-over-slide u-shading u-over-slide-3">
-                    <h3 class="u-gallery-heading"></h3>
-                    <p class="u-gallery-text"></p>
-                  </div>
-                </div>
-   
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section class="u-clearfix u-palette-5-light-2 u-section-44444" id="carousel_ba9e">
-          <div class="u-custom-color-1 u-expanded-width u-shape u-shape-rectangle u-shape-1"></div>
-          <img src="/MVC/public/images/IMG_0322.jpg" alt="" class="u-image u-image-default u-opacity u-opacity-95 u-image-1" data-image-width="1600" data-image-height="1067">
-          <div id="carousel-ab56" data-interval="5000" data-u-ride="carousel" class="u-carousel u-slider u-slider-1">
-            <ol class="u-absolute-hcenter u-carousel-indicators u-carousel-indicators-1">
-              <li data-u-target="#carousel-ab56" class="u-active u-grey-30 u-shape-circle" data-u-slide-to="0" style="width: 10px; height: 10px;"></li>
-              <li data-u-target="#carousel-ab56" class="u-grey-30 u-shape-circle" data-u-slide-to="1" style="width: 10px; height: 10px;"></li>
-            </ol>
-            <div class="u-carousel-inner" role="listbox">
-              <div class="u-active u-carousel-item u-container-style u-slide u-white u-carousel-item-1">
-                <div class="u-container-layout u-valign-top u-container-layout-1">
-                  <h2 class="u-text u-text-custom-color-1 u-text-11">01</h2>
-                  <p class="u-large-text u-text u-text-variant u-text-22">Auctor augue mauris augue neque gravida in fermentum et. Feugiat in fermentum posuere.<br>
-                    <br>Image from <a href="https://www.freepik.com/photos/music" class="u-border-1 u-border-palette-2-base u-btn u-button-style u-none u-text-body-color u-btn-1">Freepik</a>
-                  </p>
-                  <a href="https://nicepage.com/k/shopping-cart-html-templates" class="u-btn u-button-style u-grey-15 u-btn-2">learn more</a>
-                </div>
-              </div>
-              <div class="u-carousel-item u-container-style u-expanded-width u-palette-5-light-3 u-slide u-carousel-item-2">
-                <div class="u-container-layout u-valign-top u-container-layout-2">
-                  <h2 class="u-text u-text-palette-1-dark-1 u-text-33">02</h2>
-                  <p class="u-large-text u-text u-text-variant u-text-44">Auctor augue mauris augue neque gravida in fermentum et. Feugiat in fermentum posuere.<br>
-                    <br>Image from <a href="https://www.freepik.com/photos/music" class="u-border-1 u-border-palette-2-base u-btn u-button-style u-none u-text-body-color u-btn-3">Freepik</a>
-                  </p>
-                  <a href="https://nicepage.com/k/home-improvement-website-templates" class="u-btn u-button-style u-grey-15 u-btn-4">learn more</a>
-                </div>
-              </div>
-            </div>
-            <a class="u-absolute-vcenter u-carousel-control u-carousel-control-prev u-grey-10 u-spacing-9 u-text-body-color u-carousel-control-1" href="#carousel-ab56" role="button" data-u-slide="prev">
-              <span aria-hidden="true">
-                <svg viewBox="0 0 477.175 477.175"><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225
-                        c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"></path></svg>
-              </span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="u-absolute-vcenter u-carousel-control u-carousel-control-next u-grey-10 u-spacing-9 u-text-body-color u-carousel-control-2" href="#carousel-ab56" role="button" data-u-slide="next">
-              <span aria-hidden="true">
-                <svg viewBox="0 0 477.175 477.175"><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5
-                        c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"></path></svg>
-              </span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-        </section>
-    
-    
-       
-      </body>
-    </html>
-    
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4"> $title </h1>
+      <h2 class="lead">$subtitle </h2>
+      <p> User $user_id, $user_name is logged in</p>
+      <hr class="my-4">
+      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+  </div>
 EOT;
     echo $text;
     require APPROOT . '/views/inc/footer.php';

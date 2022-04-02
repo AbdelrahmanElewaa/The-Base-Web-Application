9a81a87cb9c6e6234fd88b146387a5afce486263
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2022-04-02 17:33:27 +0200

    dropdown menu

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,9 +5,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script type="text/javascript" src="Scripts/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="Scripts/bootstrap.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="undefined" crossorigin="anonymous">
   <link rel="stylesheet" href="<?php echo URLROOT;?>css/bootstrap.min.css">
+  <script src="<?php echo URLROOT; ?>js/main.js"></script>
   <link rel="stylesheet" href="<?php echo URLROOT; ?>css/style.css">
   <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/"> -->
   <title><?php echo SITENAME; ?></title>
--- app/views/inc/sidebar.php
+++ app/views/inc/sidebar.php
@@ -60,7 +60,30 @@
               Year-end sale
             </a>
           </li>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <?php if (isset($_SESSION['user_id'])) {
+              echo $_SESSION['user_name'];
+            } else {
+              echo 'User';
+            }
+            ?>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <?php if (isset($_SESSION['user_id'])) : ?>
+              <li><a class="dropdown-item" href="users/logout">Logout</a></li>
+            <?php else : ?>
+              <li><a class="dropdown-item" href="<?php echo URLROOT . 'users/login'; ?>">Login</a></li>
+              <li><a class="dropdown-item" href="<?php echo URLROOT . 'users/register'; ?>">Sign Up</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+            <?php endif; ?>
+          </ul>
+        </li>
+
         </ul>
+        
         <br>
         <br>
         <br>

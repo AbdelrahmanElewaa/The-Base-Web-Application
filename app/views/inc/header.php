b20791edfd079c8e0617ab983fbaaa9089212fbe
Abdelrahman Elewa <abdelrahmanelewa6@gmail.com>
2022-04-03 02:55:22 +0200

    chat template added

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -10,8 +10,11 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="undefined" crossorigin="anonymous">
   <link rel="stylesheet" href="<?php echo URLROOT;?>css/bootstrap.min.css">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
   <script src="<?php echo URLROOT; ?>js/main.js"></script>
   <link rel="stylesheet" href="<?php echo URLROOT; ?>css/style.css">
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>css/chat.css">
+  
   <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/"> -->
   <title><?php echo SITENAME; ?></title>
   <style type="text/css">/* Chart.js */

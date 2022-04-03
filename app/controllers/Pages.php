04d99b00391d080a9a387ebd8638d207fa50fc56
ChristineAshraf <97855069+ChristineAshraf@users.noreply.github.com>
2022-04-03 02:26:02 +0200

    Update Pages.php

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -18,7 +18,14 @@ class Pages extends Controller
         $aboutView->output();
     }
 
-
+     public function login()
+    {
+        $viewPath = VIEWS_PATH . 'pages/login.php';
+        require_once $viewPath;
+        $loginView = new Login($this->getModel(), $this);
+        $loginView->output();
+    }
+    
     public function contact()
     {
         $viewPath = VIEWS_PATH . 'pages/contact.php';
@@ -27,7 +34,39 @@ class Pages extends Controller
         $contactView->output();
     }
 
- 
+   public function plan()
+    {
+        $viewPath = VIEWS_PATH . 'pages/plan.php';
+        require_once $viewPath;
+        $planView = new Plan($this->getModel(), $this);
+        $planView->output();
+    }
+
+    public function work()
+    {
+        $viewPath = VIEWS_PATH . 'pages/work.php';
+        require_once $viewPath;
+        $workView = new Work($this->getModel(), $this);
+        $workView->output();
+    }
+    
+    public function plandisplay()
+    {
+        $viewPath = VIEWS_PATH . 'pages/plandisplay.php';
+
+        require_once $viewPath;
+        $plandisplayView = new Plandisplay($this->getModel(), $this);
+        $plandisplayView->output();
+    }
+    
+    public function workoutdisplay()
+    {
+        $viewPath = VIEWS_PATH . 'pages/workoutdisplay.php';
+        require_once $viewPath;
+        $workoutdisplayView = new Workoutdisplay($this->getModel(), $this);
+        $workoutdisplayView->output();
+    } 
+    
     public function video()
     {
         $viewPath = VIEWS_PATH . 'pages/video.php';
@@ -35,6 +74,7 @@ class Pages extends Controller
         $videoView = new Video($this->getModel(), $this);
         $videoView->output();
     }
+    
     public function chat()
     {
         $viewPath = VIEWS_PATH . 'pages/chat.php';

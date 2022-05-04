4176e9ac41930a700777fecc9dad9f26b6607334
mariam1502 <81592431+mariam1502@users.noreply.github.com>
2022-05-04 02:32:18 +0200

    Add files via upload

--- app/models/PlanModel.php
+++ app/models/PlanModel.php
@@ -3,9 +3,97 @@ require_once 'UserModel.php';
 class PlanModel extends UserModel
 {
     public  $title = 'User Login Page';
+	protected $date;
+    protected $bname;
+
+    protected $bd;
+    protected $lname;
+    protected $ld;
+    protected $dname;
+    protected $dd;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->date    = '';
+        $this->bname= '';
+
+        $this->bd  = '';
+        $this->lname = '';
+        $this->ld = '';
+        $this->dname = '';
+        $this->dd= '';
+
+    }
+	public function getdate()
+    {
+        return $this->date;
+    }
+    public function setdate($d)
+    {
+        $this->date = $d;
+    }
+
+
+    public function getbname()
+    {
+        return $this->bname;
+    }
+    public function setbname($b)
+    {
+        $this->bname = $b;
+    }
+
+
+    public function getbd()
+    {
+        return $this->bd;
+    }
+    public function setbd($b)
+    {
+        $this->bd = $b;
+    }
+
+    public function getlname()
+    {
+        return $this->lname;
+    }
+    public function setlname($l)
+    {
+        $this->lname = $l;
+    }
+    public function getld()
+    {
+        return $this->ld;
+    }
+
+    public function setld($l)
+    {
+        $this->ld = $l;
+    }
+    public function getdname()
+    {
+        return $this->dname;
+    }
+    public function setdname($d)
+    {
+        $this->dname = $d;
+    }
+    public function getdd()
+    {
+        return $this->dd;
+    }
+    public function setdd($d)
+    {
+        $this->dd = $d;
+    }
+
+
 
     public function plan()
     {
+	$this->dbh->query('select * from `nutrition`') ;
+		return $this->dbh->resultSet();
 
     }
 }

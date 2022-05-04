b82ed7cc4313527ab9bf742e938c0ae2c1a26120
mariam1502 <81592431+mariam1502@users.noreply.github.com>
2022-05-04 02:33:40 +0200

    Add files via upload

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -18,14 +18,14 @@ class Pages extends Controller
         $aboutView->output();
     }
 
-     public function login()
+    public function login()
     {
         $viewPath = VIEWS_PATH . 'pages/login.php';
         require_once $viewPath;
         $loginView = new Login($this->getModel(), $this);
         $loginView->output();
     }
-    
+
     public function contact()
     {
         $viewPath = VIEWS_PATH . 'pages/contact.php';
@@ -34,13 +34,95 @@ class Pages extends Controller
         $contactView->output();
     }
 
-   public function plan()
+
+
+
+
+
+    public function plan()
     {
+        $displaynutritionModel = $this->getModel();
+
+    //     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    //     // $displaynutritionModel->getdate();
+    //     // $registerModel->setName(trim($_POST['lastname']));
+    //     // $displaynutritionModel->getbname();
+    //     // $displaynutritionModel->getbd();
+    //     // $displaynutritionModel->getlname();
+    //     // $displaynutritionModel->getld();
+    //     // $displaynutritionModel->getdname();
+    //     // $displaynutritionModel->getdd();
+
+
+
+
+    //     $result =  $displaynutritionModel->plan();
+    //     // $displaynutritionModel->setbname()=$result[0]->breakfast;
+    //     $displaynutritionModel->setdate($result[0]->date);
+
+    //     // $displaynutritionModel->setbname()=$result['breakfast'];
+
+
+
+    
+
+        if ($result = $displaynutritionModel->plan()) 
+        {
+            $breakfastArray=array();
+            $x=0;
+            while(isset($result[$x]->date) )
+            {
+                // echo"<h1>hello</h1>";
+            $displaynutritionModel->setdate($result[$x]->date);
+            // $date=$displaynutritionModel->getdate();
+            // echo"<h1>$date</h1>";
+            $displaynutritionModel->setbname($result[$x]->breakfast);
+            $displaynutritionModel->setlname($result[$x]->lunch);
+            $displaynutritionModel->setdname($result[$x]->dinner);
+
+
+            // $breakfastArray[$x]=$b;
+            $b[$x]=$result[$x]->breakfast;
+            $l[$x]=$result[$x]->lunch;
+            $d[$x]=$result[$x]->dinner;
+            $DATE[$x]=$result[$x]->date;
+
+            // $LunchArray[$x]=$b;
+
+            $x++;
+            }
+            $_SESSION['array_to_saveB'] = $b;
+            $_SESSION['array_to_saveL'] = $l;
+            $_SESSION['array_to_saveD'] = $d;
+            $_SESSION['DATE'] = $DATE;
+
+
+
+
+
+            // echo "<h1>$date</h1>";
+            //header('location: ' . URLROOT . 'users/login');
+            // flash('Nutrition plan_success', 'nutrition plan is viwed successfully');
+            // redirect('pages/addnutrition.php');
+            
+        } else {
+            die('Error in adding nutrition');
+        }
+    // }
+
+
         $viewPath = VIEWS_PATH . 'pages/plan.php';
         require_once $viewPath;
         $planView = new Plan($this->getModel(), $this);
         $planView->output();
+
+        
     }
+
+
+
+
 
     public function work()
     {
@@ -49,7 +131,11 @@ class Pages extends Controller
         $workView = new Work($this->getModel(), $this);
         $workView->output();
     }
-    
+
+
+
+
+
     public function plandisplay()
     {
         $viewPath = VIEWS_PATH . 'pages/plandisplay.php';
@@ -58,15 +144,23 @@ class Pages extends Controller
         $plandisplayView = new Plandisplay($this->getModel(), $this);
         $plandisplayView->output();
     }
-    
+
+
+
+
+
     public function workoutdisplay()
     {
         $viewPath = VIEWS_PATH . 'pages/workoutdisplay.php';
         require_once $viewPath;
         $workoutdisplayView = new Workoutdisplay($this->getModel(), $this);
         $workoutdisplayView->output();
-    } 
+    }  
     
+    
+
+
+
     public function video()
     {
         $viewPath = VIEWS_PATH . 'pages/video.php';
@@ -74,7 +168,11 @@ class Pages extends Controller
         $videoView = new Video($this->getModel(), $this);
         $videoView->output();
     }
-    
+
+
+
+
+
     public function chat()
     {
         $viewPath = VIEWS_PATH . 'pages/chat.php';
@@ -82,4 +180,62 @@ class Pages extends Controller
         $chatView = new Chat($this->getModel(), $this);
         $chatView->output();
     }
+
+
+
+
+
+    public function addnutrition()
+    {
+        $addnutritionModel = $this->getModel();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Process form
+            $addnutritionModel->setdate(trim($_POST['date']));
+            // $registerModel->setName(trim($_POST['lastname']));
+            $addnutritionModel->setbname(trim($_POST['bname']));
+            $addnutritionModel->setbd(trim($_POST['bd']));
+            $addnutritionModel->setlname(trim($_POST['lname']));
+            $addnutritionModel->setld(trim($_POST['ld']));
+            $addnutritionModel->setdname(trim($_POST['dname']));
+            $addnutritionModel->setdd(trim($_POST['dd']));
+            
+
+
+
+            if ($addnutritionModel->addNut()) {
+                //header('location: ' . URLROOT . 'users/login');
+                flash('register_success', 'nutrition plan is added successfully');
+                redirect('pages/addnutrition.php');
+            } else {
+                die('Error in adding nutrition');
+            }
+        }
+        $viewPath = VIEWS_PATH . 'pages/addnutrition.php';
+        require_once $viewPath;
+        $aboutView = new addnutrition($this->getModel(), $this);
+        $aboutView->output();
+    }
+
+
+
+
+
+
+    // function displayNut()
+    // {
+
+    //     $viewPath = VIEWS_PATH . 'pages/addnutrition.php';
+    //     require_once $viewPath;
+    //     $aboutView = new addnutrition($this->getModel(), $this);
+    //     $aboutView->output();
+    
+    // }
+
+
+
+  
+    
+
+
+
 }

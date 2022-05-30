618edada4c3c1c3bdde3f641af01940f0c91ccf8
ChristineAshraf <97855069+ChristineAshraf@users.noreply.github.com>
2022-05-31 00:39:15 +0200

    Update Pages.php

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -589,7 +589,69 @@ class Pages extends Controller
         $deleteworkoutView->output();
     }
 
-    
+     public function editnutrition()
+    {
+        $editnutritionModel = $this->getModel();
+        $editnutritionModel->setnutritionID($_GET['nutritionID']);
+        // header('Location: http://localhost/mvc/public/pages/editnutrition?nutritionID='.$_GET['nutritionID'] );
+
+        if($editnutritionModel->getRow()){
+
+            //  $dataa=$editnutritionModel->getRow();
+          
+           $viewPath =  VIEWS_PATH . 'pages/editnutrition.php';
+            require_once $viewPath;
+            $editnutritionView = new editnutrition($this->getModel(), $this);
+            $editnutritionView->output();
+        }
+        else{
+            die('Error in Edit nutrition plan');
+        }
+     
+       
+    }
+
+
+
+    public function updatenutrition(){
+
+        $editnutritionModel = $this->getModel();
+
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $editnutritionModel->setnutritionID($_POST['nutritionID']);
+            $editnutritionModel->setdate(trim($_POST['date']));
+            $editnutritionModel->setbname(trim($_POST['bname']));
+            $editnutritionModel->setbd(trim($_POST['bd']));
+            $editnutritionModel->setlname(trim($_POST['lname']));
+            $editnutritionModel->setld(trim($_POST['ld']));
+            $editnutritionModel->setdname(trim($_POST['dname']));
+            $editnutritionModel->setdd(trim($_POST['dd']));
+            $editnutritionModel->setuserid(trim($_POST['userid']));
+
+
+            if ($editnutritionModel->update()) {
+                flash('register_success', 'Nutrition plan is Updated successfully');
+                redirect('pages/try');
+            } else {
+                die('Error in update nutrition plan');
+            }
+
+        } 
+    }
+
+    public function deleteclient()
+    {
+        $deleteclientModel = $this->getModel();
+         $deleteclientModel->setID($_GET['id']);
+        $deleteclientModel->deleteclient();
+
+
+        $viewPath = VIEWS_PATH . 'pages/deleteclient.php';
+        require_once $viewPath;
+        $deleteclientView = new deleteclient($this->getModel(), $this);
+        $deleteclientView->output();
+    }
 
 
 

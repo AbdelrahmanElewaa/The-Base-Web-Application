05d6e2b0f0296a49ce57af261355f1c306a6640b
mariam1502 <81592431+mariam1502@users.noreply.github.com>
2022-05-31 13:16:45 +0200

    Update navbar.php

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,51 +1,47 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="<?php echo URLROOT . 'public'; ?>"><?php echo SITENAME; ?></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?php echo URLROOT . 'public'; ?>">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo URLROOT . 'pages/about'; ?>">About Us</a>
-		          
-
-        </li>
-		<li class="nav-item">  <a class="nav-link" href="<?php echo URLROOT . 'pages/contact'; ?>">Contact</a>
-         </li>
-         </li>
-		<li class="nav-item">  <a class="nav-link" href="<?php echo URLROOT . 'pages/viewusers'; ?>">view users</a>
-         </li>
-		 <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <?php if (isset($_SESSION['user_id'])) {
-              echo $_SESSION['user_name'];
-            } else {
-              echo 'User';
-            }
-            ?>
+<header class="u-align-right u-clearfix u-custom-color-2 u-header u-header" id="sec-24d8"><div class="u-clearfix u-sheet u-sheet-1">
+      <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+        <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+          <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+            <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+            <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <?php if (isset($_SESSION['user_id'])) : ?>
-              <li><a class="dropdown-item" href="users/logout">Logout</a></li>
-            <?php else : ?>
-              <li><a class="dropdown-item" href="<?php echo URLROOT . 'users/login'; ?>">Login</a></li>
-              <li><a class="dropdown-item" href="<?php echo URLROOT . 'users/register'; ?>">Sign Up</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-            <?php endif; ?>
-          </ul>
-        </li>
+        </div>
+        <div class="u-custom-menu u-nav-container">
+          <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/Index'; ?>" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/about'; ?>" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/contact'; ?>" style="padding: 10px 20px;">Contact</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/login'; ?>" style="padding: 10px 20px;">Login</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/work'; ?>" style="padding: 10px 20px;">Workout</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/chat?id=&selected='; ?>" style="padding: 10px 20px;">Chat</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/plan'; ?>" style="padding: 10px 20px;">plan</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/ViewUsers'; ?>" style="padding: 10px 20px;">ViewUsers</a>
 
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+</li></ul>
+        </div>
+        <div class="u-custom-menu u-nav-container-collapse">
+          <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+            <div class="u-inner-container-layout u-sidenav-overflow">
+              <div class="u-menu-close"></div>
+              <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="<?php echo URLROOT . 'pages/about'; ?>" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="<?php echo URLROOT . 'pages/contact'; ?>" style="padding: 10px 20px;">Contact </a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="<?php echo URLROOT . 'pages/plan'; ?>" style="padding: 10px 20px;">plan</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="<?php echo URLROOT . 'pages/work'; ?>" style="padding: 10px 20px;">Workout</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link " href="<?php echo URLROOT . 'pages/login'; ?>" style="padding: 10px 20px;">Login</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link " href="<?php echo URLROOT . 'pages/chat?id=&selected='; ?>" style="padding: 10px 20px;">Chat</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link " href="<?php echo URLROOT . 'pages/ViewUsers'; ?>" style="padding: 10px 20px;">ViewUsers</a>
+
+
+
+
+</li></ul>
+            </div>
+          </div>
+          <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+        </div>
+      </nav>
+      <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="1046" data-image-height="149">
+        <img src="/MVC/public/images/Layer1.png" class="u-logo-image u-logo-image-1">
+      </a>
+    </div></header> 

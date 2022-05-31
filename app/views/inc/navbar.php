905054198d1120672f74f5d2049710abfc444548
mariam1502 <81592431+mariam1502@users.noreply.github.com>
2022-06-01 01:34:23 +0200

    Update navbar.php

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,4 +1,4 @@
-<header class="u-align-right u-clearfix u-custom-color-2 u-header u-header" id="sec-24d8"><div class="u-clearfix u-sheet u-sheet-1">
+<header  class="u-align-right u-clearfix u-custom-color-2 u-header u-header" id="sec-24d8"><div class="u-clearfix  u-sheet-1">
       <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
         <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
           <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
@@ -8,7 +8,8 @@
           </a>
         </div>
         <div class="u-custom-menu u-nav-container">
-          <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/Index'; ?>" style="padding: 10px 20px;">Home</a>
+          <ul class="u-nav u-unstyled u-nav-1" style="right:100px">
+            <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/Index'; ?>" style="padding: 10px 20px;">Home</a>
 </li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/about'; ?>" style="padding: 10px 20px;">About</a>
 </li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/contact'; ?>" style="padding: 10px 20px;">Contact</a>
 </li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-5-dark-1 u-text-hover-palette-5-dark-2 u-text-palette-5-light-2" href="<?php echo URLROOT . 'pages/login'; ?>" style="padding: 10px 20px;">Login</a>
@@ -23,25 +24,13 @@
           <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
             <div class="u-inner-container-layout u-sidenav-overflow">
               <div class="u-menu-close"></div>
-              <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Home.html" style="padding: 10px 20px;">Home</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="<?php echo URLROOT . 'pages/about'; ?>" style="padding: 10px 20px;">About</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="<?php echo URLROOT . 'pages/contact'; ?>" style="padding: 10px 20px;">Contact </a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="<?php echo URLROOT . 'pages/plan'; ?>" style="padding: 10px 20px;">plan</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="<?php echo URLROOT . 'pages/work'; ?>" style="padding: 10px 20px;">Workout</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link " href="<?php echo URLROOT . 'pages/login'; ?>" style="padding: 10px 20px;">Login</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link " href="<?php echo URLROOT . 'pages/chat?id=&selected='; ?>" style="padding: 10px 20px;">Chat</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link " href="<?php echo URLROOT . 'pages/ViewUsers'; ?>" style="padding: 10px 20px;">ViewUsers</a>
 
-
-
-
-</li></ul>
             </div>
           </div>
           <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
         </div>
       </nav>
-      <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="1046" data-image-height="149">
+      <a href="<?php echo URLROOT . 'pages/about'; ?>" class="u-image u-logo u-image-1" data-image-width="1046" data-image-height="149" >
         <img src="/MVC/public/images/Layer1.png" class="u-logo-image u-logo-image-1">
       </a>
     </div></header> 

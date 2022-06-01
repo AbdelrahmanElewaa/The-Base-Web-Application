550a1fa837e2f08d5df5f12d2dc62aadc9c55a8e
Abdelrahman Elewa <95569208+AbdelrahmanElewaa@users.noreply.github.com>
2022-06-02 00:42:42 +0200

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -9,7 +9,7 @@ define('DB_NAME', 'base');
 //echo dirname(dirname(__FILE__));
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-// define('URLROOT', 'http://localhost/mvc/public/');
+define('URLROOT', 'http://localhost/mvc/public/');
 // Site Name
 define('SITENAME', 'MIU Blog MVC');
 

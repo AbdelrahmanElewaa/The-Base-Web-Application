783bcdc0801c1fefa9239cacc50746e60b321871
Abdelrahman Elewa <95569208+AbdelrahmanElewaa@users.noreply.github.com>
2022-06-02 17:50:27 +0200

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
 // DB Params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'base');
+define('DB_HOST', 'db-mysql-sfo3-98166-do-user-11681204-0.b.db.ondigitalocean.com');
+define('DB_USER', 'doadmin');
+define('DB_PASS', 'AVNS_kagIXRxFVPXeH1_');
+define('DB_NAME', 'defaultdb');
 
 // App Root
 //echo dirname(dirname(__FILE__));
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/mvc/public/');
+define('URLROOT', 'http://http://137.184.122.185/public/');
 // Site Name
 define('SITENAME', 'MIU Blog MVC');
 
@@ -17,7 +17,7 @@ define('APP_VERSION', '1.2');
 
 //public pages path
 define('VIEWS_PATH', '../app/views/');
-define('IMGROOT', 'http://localhost/mvc/public/images/');
+define('IMGROOT', 'http://http://137.184.122.185/deploy.php/public/images/');
 
 
 

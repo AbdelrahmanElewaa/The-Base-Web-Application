32a4fa23538ec4786195abfbf07d2f23bf8e1ee2
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2022-06-03 20:38:48 +0200

    Revert "Update config.php"
    This reverts commit 391d7caf1fb62afcad25a45b16399e76a7bc0dca.

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 // DB Params
-define('DB_HOST', 'localhost');
+define('DB_HOST', '137.184.122.185');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'base');
@@ -9,16 +9,12 @@ define('DB_NAME', 'base');
 //echo dirname(dirname(__FILE__));
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/mvc/public/');
+define( '137.184.122.185/public');
 // Site Name
-define('SITENAME', 'MIU Blog MVC');
+define('SITENAME', 'html');
 
 define('APP_VERSION', '1.2');
 
 //public pages path
 define('VIEWS_PATH', '../app/views/');
 define('IMGROOT', 'http://localhost/mvc/public/images/');
-
-
-
-
